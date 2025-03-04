f904f563433536963165c77680d58fd891251655
Joseph Nyingi <josenyingi@gmail.com>
2025-03-04 11:40:37 +0900

    updated the pubspec yaml to fetch data

--- mobile/lib/services/weather_services.dart
+++ mobile/lib/services/weather_services.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class WeatherService {
+  static const String _apiUrl = "http://127.0.0.1:8000/predict/";
+
   static Future<Map<String, dynamic>> getWeather(String date, String location) async {
-    // Simulated API response
-    await Future.delayed(Duration(seconds: 1));  // Simulate network delay
-    return {
-      "temperature_prediction": 28.5,
-      "rain_prediction": 5.2,
-    };
+    final response = await http.post(
+      Uri.parse(_apiUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"date": date, "location": location}),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to fetch weather data');
+    }
   }
 }
